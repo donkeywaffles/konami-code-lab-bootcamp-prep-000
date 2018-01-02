df9757bf8d93c61d6345d8543b80adcb4acc544c
Jonathan Valdesuso <jbvaldesuso@gmail.com>
2018-01-02 22:01:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,10 +3,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index = 0;
   const body = document.querySelector('body');
-  var isCode = true;
-  while (isCode) {
-    body.addEventListener('keydown', function(e) {
-    const key = e.detail || e.which;
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
     if (key === code[index]) {
       console.log(key);
       index++;
@@ -16,8 +14,7 @@ function init() {
       }
     } else {
       index = 0;
-    }  
-  }
+    }
 
   });
 }
